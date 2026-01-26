3c68a17e88a32f428d68012d38118523a5bc8ec2
NH-Bappy <naimulbappy2207@gmail.com>
2026-01-26 19:55:19 +0600

    working on home page setup

---
+++ src/helpers/assetsProbider.js
@@ -0,0 +1,13 @@
+import ad from '../assets/homeAssets/Black.png';
+import logo from '../assets/homeAssets/Logo.png';
+import bannerLeft from '../assets/homeAssets/bannerSlider3.png';
+import bannerRightTop from '../assets/homeAssets/bannerSlider2.png';
+import bannerRightBottom from '../assets/homeAssets/bannerSlider3.png';
+
+export const assets = {
+    ad,
+    logo,
+    bannerLeft,
+    bannerRightTop,
+    bannerRightBottom,
+}
---
+++ src/helpers/iconProvider.js
@@ -0,0 +1,11 @@
+import { FaTwitter, FaFacebook, FaPinterestP, FaReddit, FaYoutube } from "react-icons/fa";
+
+
+
+export const icon = {
+    twitter: <FaTwitter />,
+    facebook: <FaFacebook />,
+    pinterestP: <FaPinterestP />,
+    reddit: <FaReddit />,
+    youtube: <FaYoutube />,
+}
