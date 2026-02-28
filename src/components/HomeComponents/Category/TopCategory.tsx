bb693917d19956d50e4c56405aa4b90bd176fce9
NH-Bappy <naimulbappy2207@gmail.com>
2026-02-28 23:42:23 +0600

    solved some problem

--- src/components/HomeComponents/Category/TopCategory.tsx
+++ src/components/HomeComponents/Category/TopCategory.tsx
@@ -4,28 +4,32 @@ import { icons } from "../../../helpers/iconProvider"
 
 const TopCategory = () => {
     const [categoryOpen , setCategoryOpen] = useState(false)
+    console.log(categoryOpen)
 
     return (
         <div>
             <Container>
-                <div className="py-3 relative">
-                    <select 
-                    name="Category" 
-                    id="Category" 
-                    className="body_sm_500 bg-gray-50 pl-2 pr-8 py-2 rounded outline-none cursor-pointer appearance-none"
-                    onChange={() => setCategoryOpen(categoryOpen)}
-                    >
-                        <option className="body_sm_500 bg-gray-50" value="category">All Category</option>
-                        <option className="body_sm_500 bg-gray-50" value="Mobile">Mobile</option>
-                        <option className="body_sm_500 bg-gray-50" value="Laptop">Laptop</option>
-                        <option className="body_sm_500 bg-gray-50" value="Tablet">Tablet</option>
-                        <option className="body_sm_500 bg-gray-50" value="Camera">Camera</option>
-                        <option className="body_sm_500 bg-gray-50" value="Accessories">Accessories</option>
-                    </select>
-                    <span 
-                    className="absolute top-1/2 -translate-1/2  left-[105px]">
-                        {categoryOpen ? icons.upArrow : icons.downArrow}
-                    </span>
+                <div className="py-3">
+                    {/* category */}
+                    <div className="relative">
+                        <select
+                            name="Category"
+                            id="Category"
+                            className="body_sm_500 bg-gray-50 pl-2 pr-8 py-2 rounded outline-none cursor-pointer appearance-none"
+                            onClick={() => setCategoryOpen(!categoryOpen)}
+                        >
+                            <option className="body_sm_500 bg-gray-50" value="category">All Category</option>
+                            <option className="body_sm_500 bg-gray-50" value="Mobile">Mobile</option>
+                            <option className="body_sm_500 bg-gray-50" value="Laptop">Laptop</option>
+                            <option className="body_sm_500 bg-gray-50" value="Tablet">Tablet</option>
+                            <option className="body_sm_500 bg-gray-50" value="Camera">Camera</option>
+                            <option className="body_sm_500 bg-gray-50" value="Accessories">Accessories</option>
+                        </select>
+                        <span
+                            className="absolute top-1/2 -translate-1/2  left-[105px]">
+                            {categoryOpen ? icons.upArrow : icons.downArrow}
+                        </span>
+                    </div>
                 </div>
             </Container>
         </div>
