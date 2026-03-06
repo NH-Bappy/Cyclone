57e40c64fcdd2de4aa30f603bdcb100dc4704c49
NH-Bappy <naimulbappy2207@gmail.com>
2026-03-06 12:09:51 +0600

    working on banner

--- src/components/HomeComponents/Banner/Banner.tsx
+++ src/components/HomeComponents/Banner/Banner.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import Container from '../../commonComponents/Container'
+import { assets } from '../../../helpers/assetsProvider'
 
 const Banner = () => {
   return (
-    <div>
+    <div className='py-10'>
         <Container>
-            
+            <div className="grid grid-cols-[2fr_1fr] gap-x-4">
+                <div className="rounded w-full h-full bg-cover">
+                      <img src={assets.bannerLeft} alt={assets.bannerLeft} />
+                </div>
+                <div className="grid grid-rows-2 gap-y-4">
+                      <div className="rounded w-full h-full bg-cover">
+                          <img src={assets.bannerRightTop} alt={assets.bannerRightTop} />
+                    </div>
+                      <div className="rounded w-full h-full bg-cover">
+                          <img src={assets.bannerRightBottom} alt={assets.bannerRightBottom} />
+                    </div>
+                </div>
+            </div>
         </Container>
     </div>
   )
--- src/helpers/assetsProvider.tsx
+++ src/helpers/assetsProvider.tsx
@@ -1,6 +1,6 @@
 import ad from '../assets/homeAssets/Black.png';
 import logo from '../assets/homeAssets/Logo.png';
-import bannerLeft from '../assets/homeAssets/bannerSlider3.png';
+import bannerLeft from '../assets/homeAssets/BannerSlider1.png';
 import bannerRightTop from '../assets/homeAssets/bannerSlider2.png';
 import bannerRightBottom from '../assets/homeAssets/bannerSlider3.png';
 
