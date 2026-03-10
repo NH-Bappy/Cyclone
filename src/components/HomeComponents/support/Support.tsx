d24c56137451a7a605d78c241623a54fd5f7fdcd
NH-Bappy <naimulbappy2207@gmail.com>
2026-03-10 23:01:13 +0600

    support part complete

--- src/components/HomeComponents/support/Support.tsx
+++ src/components/HomeComponents/support/Support.tsx
@@ -1,14 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from '../../commonComponents/Container'
+import { assets } from '../../../helpers/assetsProvider'
+
+
 
 const Support = () => {
-  return (
-    <div>
-        <Container>
-            <h2>hello</h2>
-        </Container>
-    </div>
-  )
+
+    const [support, setSupport] = React.useState([
+        {
+            id: 1,
+            name: "Fasted Delivery",
+            description: "Delivery in 24/H",
+            icon: assets.packages,
+        },
+        {
+            id: 2,
+            name: "24 Hours Return",
+            description: "100% money-back guarantee",
+            icon: assets.trophy,
+        },
+        {
+            id: 3,
+            name: "Secure Payment",
+            description: "Your money is safe",
+            icon: assets.creditCard,
+        },
+        {
+            id: 4,
+            name: "Support 24/7",
+            description: "Live contact/message",
+            icon: assets.headPhone,
+        },
+    ])
+
+
+
+
+
+    return (
+
+
+
+        <div className='py-20'>
+            <Container>
+                <div className="border border-gray-100 px-10 py-6 grid grid-cols-4 gap-x-5 rounded">
+
+                    {support.map((item) => (
+                        <div
+                            key={item.id}
+                            className={support.length == item.id ? "flex items-center gap-x-3" : "flex items-center gap-x-3 border-r border-r-gray-100 cursor-pointer"}
+                        >
+                            <img
+                                src={item.icon}
+                                alt="iconImage"
+                                className='text-gray-900 object-center'
+                            />
+                            <div className="">
+                                <h2 className='text-lg label3 text-gray-900 pb-2'>
+                                    {item.name}
+                                </h2>
+                                <p className='text-gray-600 body_sm_400'>
+                                    {item.description}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+
+
+
+
+
+                </div>
+            </Container>
+        </div>
+    )
 }
 
 export default React.memo(Support)
--- src/helpers/assetsProvider.tsx
+++ src/helpers/assetsProvider.tsx
@@ -6,6 +6,10 @@ import bannerLeft3 from '../assets/homeAssets/banner3.jpg';
 import bannerLeft4 from '../assets/homeAssets/banner4.jpg';
 import bannerRightTop from '../assets/homeAssets/bannerSlider2.png';
 import bannerRightBottom from '../assets/homeAssets/bannerSlider3.png';
+import creditCard from '../assets/homeAssets/CreditCard.png';
+import headPhone from '../assets/homeAssets/Headphones.png';
+import packages from '../assets/homeAssets/Package.png';
+import trophy from '../assets/homeAssets/Trophy.png';
 
 export const assets = {
     ad,
@@ -16,4 +20,8 @@ export const assets = {
     bannerLeft4,
     bannerRightTop,
     bannerRightBottom,
+    creditCard,
+    headPhone,
+    packages,
+    trophy,
 }
