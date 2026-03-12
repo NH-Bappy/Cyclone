7a14dfd4d857a0916b543435f07d49ce829306ba
NH-Bappy <naimulbappy2207@gmail.com>
2026-03-13 01:20:53 +0600

    working on feature product

--- src/components/HomeComponents/FeaturedProducts/FeaturedProducts.tsx
+++ src/components/HomeComponents/FeaturedProducts/FeaturedProducts.tsx
@@ -1,20 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from '../../commonComponents/Container'
 import { assets } from '../../../helpers/assetsProvider'
 
 const FeaturedProducts: React.FC = () => {
-  return (
-    <div className='py-10'>
-        <Container>
-            <div className="lg:grid lg:grid-cols-[1fr_3fr] gap-3">
-                <div className="">
-                    <img src={assets.ProductBanner} alt={"banner picture"} className='shadow-2xl w-full h-full object-cover' />
+
+
+    const [productList] = useState([
+        {
+            id: 1,
+            name: "All Product",
+            slug: "AllProduct"
+        },
+        {
+            id: 2,
+            name: "Smart Phone",
+            slug: "SmartPhone"
+        },
+        {
+            id: 3,
+            name: "Laptop",
+            slug: "Laptop"
+        },
+        {
+            id: 4,
+            name: "Headphone",
+            slug: "Headphone"
+        },
+        {
+            id: 5,
+            name: "TV",
+            slug: "TV"
+        },
+    ])
+
+
+
+
+    return (
+        <div className='py-10'>
+            <Container>
+                <div className="lg:grid lg:grid-cols-[1fr_3fr] gap-x-4">
+                    <div className="">
+                        <img src={assets.ProductBanner} alt={"banner picture"} className='shadow-2xl w-full h-full object-cover' />
+                    </div>
+                    <div className="grid grid-rows-[0.5fr_4fr] rounded">
+                        <div className="grid grid-cols-2">
+                            <h2 className='heading3 text-gray-900'>Featured Products</h2>
+                            <div className="">
+
+                                <ul className='flex items-center gap-x-5'>
+                                    {productList.map((item) => (
+                                        <li
+                                            key={item.id}
+                                            className='body_sm_400 text-gray-900 CustomStyle'
+                                        >
+                                            {item.name}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="">low tir</div>
+                    </div>
                 </div>
-                <div className="">2</div>
-            </div>
-        </Container>
-    </div>
-  )
+            </Container>
+        </div>
+    )
 }
 
 export default React.memo(FeaturedProducts)
