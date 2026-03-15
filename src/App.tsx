be029d258e54e54b26f153f4ab5af08ef58c1cef
NH-Bappy <naimulbappy2207@gmail.com>
2026-03-15 23:42:18 +0600

    setup transtack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./pages/Home";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 const App = () => {
+  // Create a client
+  const queryClient = new QueryClient()
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+
     </>
   );
 };
