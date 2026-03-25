c5df5b9bdcb18c4e76c19b242311ba9cef5d5350
NH-Bappy <naimulbappy2207@gmail.com>
2026-03-25 23:38:36 +0600

    working on new part

--- src/components/HomeComponents/FeaturedProducts/FeaturedProducts.tsx
+++ src/components/HomeComponents/FeaturedProducts/FeaturedProducts.tsx
@@ -7,11 +7,13 @@ import Product from '../../commonComponents/Product'
 
 
 const FeaturedProducts: React.FC = () => {
+    const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+
 
     // category show
     const { data: categoryData, isLoading: categoryLoading, error: categoryError } = useApiCategory();
 
-    const { data , isLoading , error } = useGetApi();
+    const { data, isLoading, error } = useGetApi();
 
     if (isLoading || categoryLoading) return <h2>Loading...</h2>;
     if (error || categoryError) return <h2>Error</h2>;
@@ -19,7 +21,12 @@ const FeaturedProducts: React.FC = () => {
     // category show
     const displayCategories = categoryData?.slice(0, 5);
 
-    const filteredProducts = data?.products?.slice(0, 8);
+    const filteredProducts = data?.products
+        ?.filter((product: any) =>
+            !selectedCategory // if no category selected, show all
+            || product.category.toLowerCase() === selectedCategory.toLowerCase()
+        ).slice(0, 8);
+
 
 
 
@@ -37,7 +44,7 @@ const FeaturedProducts: React.FC = () => {
                         <img src={assets.ProductBanner} alt={"banner picture"} className='shadow-2xl w-full h-full object-cover' />
                     </div>
                     <div className="grid grid-rows-[0.3fr_4fr] rounded">
-                        
+
                         {/* row top */}
                         <div className="flex items-start pb-6 justify-between">
                             <h2 className='heading3 text-gray-900'>Featured Products</h2>
@@ -45,19 +52,25 @@ const FeaturedProducts: React.FC = () => {
 
                                 <ul className='flex items-center gap-x-5'>
                                     {displayCategories?.map((cat: any) => (
-                                        <li key={cat.id} className='body_sm_400 text-gray-900 CustomStyle'>
+                                        <li
+                                            key={cat.id}
+                                            className={`body_sm_400 text-gray-900 CustomStyle cursor-pointer 
+                        ${selectedCategory?.toLowerCase() === cat.name.toLowerCase() ? 'font-bold text-primary-500' : ''}`}
+                                            onClick={() => setSelectedCategory(cat.name)}
+                                        >
                                             {cat.name}
                                         </li>
                                     ))}
                                 </ul>
-                                <button className='flex gap-x-1 items-center text-primary-500 body_sm_400'>
+                                <button className='flex gap-x-1 items-center text-primary-500 body_sm_400'
+                                    onClick={() => setSelectedCategory(null)}>
                                     Browse All Product
-                                <span className='text-3xl'>{icons.rightArrow}</span>
+                                    <span className='text-3xl'>{icons.rightArrow}</span>
                                 </button>
 
                             </div>
                         </div>
-                        
+
                         {/* row bottom */}
 
 
--- src/content/index.ts
+++ src/content/index.ts
@@ -67,3 +67,4 @@ export const shopCategory = [
     img: assets.drones,
   },
 ]
+
